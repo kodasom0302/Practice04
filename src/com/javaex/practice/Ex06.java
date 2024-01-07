71d4877c38a90c74d640f586183a86022920eefc
dasom <dasom589@gmail.com>
2024-01-07 23:35:31 +0900

    2024-01-07

---
+++ src/com/javaex/practice/Ex06.java
@@ -0,0 +1,17 @@
+package com.javaex.practice;
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		
+		char[] c={'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l'};
+		System.out.println(c);
+		char[] b;
+		char[] b=char[] c;
+		b[5]=',';
+		b[8]=',';
+		b[10]=',';
+
+	}
+
+}

4dd1bd23f421caacccae81d803dda23dd2113c81
dasom <dasom589@gmail.com>
2024-01-07 23:35:13 +0900

    2024-01-07

---
+++ src/com/javaex/practice/Ex04.java
@@ -0,0 +1,27 @@
+package com.javaex.practice;
+
+public class Ex04 {
+
+/*	
+다음 주어진 배열 데이터를 이용하여 3의 배수의 개수와 배수의 합을 아래와 같이 출력하는 프로
+그램을 작성하세요.
+
+	public static void main(String[] args) {
+		
+		int[] data= {1,3,5,8,9,11,15,18,19,20,30,31,33};
+		int sum=0;
+		int count=0;
+		
+		for (int i=0; i<data.length; i++) {
+			if (data[i]%3==0) {
+				sum=sum+data[i];
+				count+=1;
+			}
+		}
+		
+		System.out.println("주어진 배열에서 3의 배수의 개수=>"+count);
+		System.out.println("주어진 배열에서 3의 배수의 합=>"+sum);
+
+	}
+*/
+}

4d800f5af25b4f3b8a862de313b6398015ced70c
dasom <dasom589@gmail.com>
2024-01-07 23:35:00 +0900

    2024-01-07

---
+++ src/com/javaex/practice/Ex03.java
@@ -0,0 +1,29 @@
+package com.javaex.practice;
+
+public class Ex03 {
+/*
+아래코드의 출력값을 예상하고 코드로 확인해 보세요.
+
+	public static void main(String[] args) {
+		
+		int[] intA= {3, 6, 9};
+		
+		int[] intB;
+		intB=intA;
+		intB[0]=20;
+		intB[2]=10;
+		
+		for (int i=0; i<intA.length; i++) {
+			System.out.println(intA[i]);
+		}
+
+	}
+
+	
+	
+
+20
+6
+10
+*/
+}

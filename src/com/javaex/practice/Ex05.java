8ca49bf888120bb4d8d136989701ef2cfc6e4762
dasom <dasom589@gmail.com>
2024-01-07 23:35:22 +0900

    2024-01-07

---
+++ src/com/javaex/practice/Ex05.java
@@ -0,0 +1,29 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex05 {
+
+/*	
+5개의 숫자를 키보드로 입력받아 평균을 구하는 프로그램을 작성하세요.
+
+	public static void main(String[] args) {
+		
+		int sum=0;
+		Scanner sc=new Scanner(System.in);
+		int[] num=new int[5];
+		
+		for (int i=0; i<num.length; i++) {
+			num[i]=sc.nextInt();
+			sum=sum+num[i];
+		}
+		
+		double result=sum/5;
+		
+		System.out.println("평균은 "+result+" 입니다.");
+		
+		sc.close();
+
+	}
+*/
+}

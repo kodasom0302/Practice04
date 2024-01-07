294972d4d64f09ee85f09693cd16d9843ca91e46
dasom <dasom589@gmail.com>
2024-01-07 23:30:49 +0900

    2024-01-07

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,30 @@
+package com.javaex.practice;
+
+public class Ex01 {
+
+/*	
+아래의 코드를 실행하면 출력결과에 오류가 발생한다. 오류 수정 후 출력결과를 예상하고 코드로
+확인해 보세요.
+
+	public static void main(String[] args) {
+		
+		int[] intArray=new int[5];
+		intArray[0]=3;
+		intArray[1]=7;
+		intArray[2]=12;
+		
+		int result=0;
+		
+		for (int i=0; i<intArray.length; i++) {
+			result=result+intArray[i];
+		}
+		System.out.println(result);
+
+	}
+	
+	
+	
+22
+i<=intArray -> i<intArray 0부터 시작하기 때문에 같다고 하면 [] 수가 1 모자람
+*/
+}

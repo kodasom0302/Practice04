ce66a2f3d1e376ad6e2957084f3ae1df2681c783
dasom <dasom589@gmail.com>
2024-01-07 23:34:12 +0900

    2024-01-07

---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,22 @@
+package com.javaex.practice;
+
+public class Ex02 {
+
+/*	??? 출력값은 맞는데 밑에 오류 뜸
+double 형 배열 을 작성하고 1.2, 3.3, 6.7 값을 순서대로 입력하고 아래와 같이 출력되도록 for문
+을 작성하세요.
+
+	public static void main(String[] args) {
+		
+		double[] doubleArray=new double[3];
+		doubleArray[0]=1.2;
+		doubleArray[1]=3.3;
+		doubleArray[2]=6.7;
+		
+		for (int i=2; i<doubleArray.length; i--) {
+			System.out.println(doubleArray[i]);
+		}
+
+	}
+*/
+}
